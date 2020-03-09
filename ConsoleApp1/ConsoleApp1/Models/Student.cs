c718471adb95232a0a684b5e826ca9f8dbef5b56
Maksym Alvan <s18511@pjwstk.edu.pl>
2020-03-09 11:22:31 +0100

    COMMIT

---
+++ ConsoleApp1/ConsoleApp1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1.Models
+{
+    public class Student
+    {
+        //prop + tabx2
+        [XmlElement(elementName:"fname")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        [XmlAttribute(attributeName: "email")]
+        public string Email { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrlist =  list
+            //set  = hashset
+            //map = dictionary
+
+
+            string file = @"Data\dane.csv";
+            FileInfo f = new FileInfo(file);
+            using (var stream = new StreamReader(f.OpenRead()))
+            {
+
+                string line = "";
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] studentWiersz = line.Split(',');
+
+                    Console.WriteLine(line);
+                }
+            }//Dispose automatycznie
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+            serializer.Serialize(writer, list);
+
+
+            //XML
+            // stream.Dispose(); // using zamiast dispose auto korzysta dispose 
+
+        }
+    }
+}
